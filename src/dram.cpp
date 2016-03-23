17ddffaadd3de7f2258a6569c98967dcab9f4638
AbhayG27 <abhayg271@gmail.com>
2016-03-24 02:27:22 +0530

    trial

--- src/dram.cpp
+++ src/dram.cpp
@@ -1,4 +1,4 @@
-#include "dram.h"
+#include <dram.h>
 dram::dram()
 {
 
@@ -17,10 +17,10 @@ int dram::read(mem_addr_t from, int count, mem_block_t* buf)
 	file.read((char*)buf, count*sizeof(mem_block_t));
 	return DRAM_READ_LATENCY;
 }
-int dram::write(mem_addr_t from, int count, mem_block_t buf)
+int dram::write(mem_addr_t from, int count, mem_block_t* buf)
 {
 	file.seekg(from);
-	file.write((char*)&buf, count*sizeof(mem_block_t));
+	file.write((char*)buf, count*sizeof(mem_block_t));
 	return DRAM_WRITE_LATENCY;
 }
 dram::~dram()
--- src/memCntrl.cpp
+++ src/memCntrl.cpp
@@ -21,7 +21,7 @@ int memcntrl::write(mem_addr_t addr, mem_word_t word)
 	mem_block_t block;
 	int lat = myDram.read(addr, 1, &block);
 	block.words[0]=word;
-	lat+=myDram.write(addr, 1, block);
+	lat+=myDram.write(addr, 1, &block);
 	return lat;
 }
 
