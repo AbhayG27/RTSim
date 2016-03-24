402f216d90271fdec292ba4997004479727dd783
AbhayG27 <abhayg271@gmail.com>
2016-03-25 02:53:40 +0530

    abhay

--- src/rt_sim.cpp
+++ src/rt_sim.cpp
@@ -15,12 +15,12 @@ int main(int argc, char * argv[])
 	/*char a = 0;
 	for (int i = 0; i < pow(2, sizeof(mem_addr_t)*8); i++)
 		myMem.writeLittle(i, 1, &a);*/
-	mem_addr_t ad = 100;
-	myMem.writeLittle(0, sizeof(mem_addr_t), &ad);
-	myMem.writeLittle(4, sizeof(mem_addr_t), &ad);
-	myMem.writeLittle(8, sizeof(mem_addr_t), &ad);
-	myMem.writeLittle(12, sizeof(mem_addr_t), &ad);
-	myMem.writeLittle(12, sizeof(mem_addr_t), &ad);
+	mem_addr_t ad = 0;
+	myMem.writeLittle(TREE_START_OFFSET_LOC, sizeof(mem_addr_t), &ad);
+	myMem.writeLittle(TREE_LEAF_START_OFFSET_LOC, sizeof(mem_addr_t), &ad);
+	myMem.writeLittle(TRIANGLE_START_OFFSET_LOC, sizeof(mem_addr_t), &ad);
+	myMem.writeLittle(RAY_START_OFFSET_LOC, sizeof(mem_addr_t), &ad);
+	myMem.writeLittle(SCRATCH_START_OFFSET_LOC, sizeof(mem_addr_t), &ad);
 	//mem_word_t temp,w = 'a';
 	buffer<ray> rays(RAY_BUF_SIZE, 1, 1);
 	buffer<precomputedRay> precomputedRays(RAY_BUF_SIZE, 1, 1);
